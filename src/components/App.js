ba751f98a45e91ade949aef9099ed261a49d9296
Milicevic <Milicevic@fidor.de>
2018-02-27 00:31:17 +0100

    refactored to pure components

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component }     from 'react';
+import React                    from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header                   from './Header';
 import Dashboard                from './Dashboard';
@@ -6,21 +6,17 @@ import CurrencyDetailsContainer from './Currency/Details/CurrencyDetailsContaine
 import Settings                 from './Settings/Settings';
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <BrowserRouter>
-          <div>
-            <Header />
-            <Route exact path="/" component={Dashboard} />
-            <Route exact path="/currency/:id" component={CurrencyDetailsContainer} />
-            <Route exact path="/settings" component={Settings} />
-          </div>
-        </BrowserRouter>
+const App = () => (
+  <div className="container">
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Route exact path="/" component={Dashboard} />
+        <Route exact path="/currency/:id" component={CurrencyDetailsContainer} />
+        <Route exact path="/settings" component={Settings} />
       </div>
-    );
-  }
-};
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
--- src/components/Currency/List/ColumnsTitle.js
+++ src/components/Currency/List/ColumnsTitle.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ColumnsTitle extends Component {
-  render() {
-    return (
-      <thead>
-        <tr>
-          <th className="text-uppercase"> Rank </th>
-          <th className="text-uppercase"> Symbol </th>
-          <th className="text-uppercase"> Price ​in ​the ​selected ​fiat ​currency </th>
-          <th className="text-uppercase"> 24 hour change </th>
-        </tr>
-      </thead>
-    );
-  }
-}
+const ColumnsTitle = () => (
+  <thead>
+    <tr>
+      <th className="text-uppercase"> Rank </th>
+      <th className="text-uppercase"> Symbol </th>
+      <th className="text-uppercase"> Price ​in ​the ​selected ​fiat ​currency </th>
+      <th className="text-uppercase"> 24 hour change </th>
+    </tr>
+  </thead>
+);
 
 export default ColumnsTitle;
